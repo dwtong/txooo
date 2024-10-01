31d88a1825c9f4f5565480fb0a74422f6daa8dde
Dan Tong <dan@dtong.nz>
2024-10-01 14:27:14 +1300

    add txo lfo controls

--- lib/mod.lua
+++ lib/mod.lua
@@ -26,6 +26,24 @@ mod.hook.register("script_post_init", "txooo init", function()
     params:add_separator("TXO")
     params:add_number("txooo_clock_ppqn", "clock ppqn", 1, 16, 4)
     for i = 1, 4 do
+        params:add_number("txooo_" .. i .. "_lfo_rate", "lfo rate " .. i, 0, 100, 0)
+        params:set_action("txooo_" .. i .. "_lfo_rate", function(value)
+            crow.ii.txo.osc_lfo(i, value * 10)
+        end)
+    end
+    for i = 1, 4 do
+        params:add_number("txooo_" .. i .. "_lfo_depth", "lfo depth " .. i, 0, 100, 0)
+        params:set_action("txooo_" .. i .. "_lfo_depth", function(value)
+            crow.ii.txo.cv(i, value / 10)
+        end)
+    end
+    for i = 1, 4 do
+        params:add_number("txooo_" .. i .. "_lfo_shape", "lfo shape " .. i, 0, 100, 0)
+        params:set_action("txooo_" .. i .. "_lfo_shape", function(value)
+            crow.ii.txo.osc_wave(i, value * 100)
+        end)
+    end
+    for i = 1, 4 do
         params:add_option("txooo_" .. i .. "_clock_rate", "clock rate " .. i, clock_options, 12)
         clock.run(function()
             while true do
